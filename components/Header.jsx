3ceb579bd6d2d81e7494d39f6b66811820e7bbeb
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-03-03 14:24:17 +0530

    User Authentication  - Created (auth), sign-in,sign-up,layout in auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 const header = () => {
-  return <div>header</div>;
+  return (
+    <div className='fixed top-0'>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default header;
